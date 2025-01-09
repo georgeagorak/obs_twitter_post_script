0f3a021f28ff610348b3cca599ae3a6e0cfa6a9a
George Gorak <73673803+georgeagorak@users.noreply.github.com>
2025-01-09 21:33:20 +0000

    Fix bug: update date constantly + better logging

--- obs_twitter_post_script.py
+++ obs_twitter_post_script.py
@@ -8,14 +8,13 @@ import twitch_info
 # from twitchAPI.helper import first
 
 sendTweet = False
-current_time = datetime.datetime.now().strftime("%d/%m/%Y at %H:%M")
 
 client = tweepy.Client(config_file.barear_token, config_file.api_key, config_file.api_secret, config_file.access_token_user, config_file.access_token_sercret_user)
 auth = tweepy.OAuthHandler(config_file.api_key, config_file.api_secret, config_file.access_token_user, config_file.access_token_sercret_user)
 api_key = tweepy.API(auth)
 
 access_token = twitch_info.get_access_token(client_id=config_file.client_id, client_secret=config_file.client_secret)
-user_id = twitch_info.get_user_id(user_name="timthetatman", client_id=config_file.client_id, acces_token=access_token)
+user_id = twitch_info.get_user_id(user_name="jurgon56", client_id=config_file.client_id, acces_token=access_token)
 
 def script_description():
       return """<center><h2>Send X post Script</h2></center>
@@ -29,18 +28,19 @@ def timer_callback():
       global sendTweet
       
       stream = twitch_info.get_stream(user_id=user_id, client_id=config_file.client_id, acces_token=access_token)
-
+      current_time = datetime.datetime.now().strftime("%d/%m/%Y at %H:%M")
       if stream == "This user is not streaming":
             print("This user is not streaming")
       else:
             print("Streaming now...")
             if obspython.obs_frontend_streaming_active() and not sendTweet:
                   sendTweet = True
-                  print("Stream is active")
+                  print("Stream is active...")
                   client.create_tweet(text=f"AY YOU! On {current_time} Nerd Just started streaming {stream['game_name']} titled ''{stream['title']}'' ┌( ͝° ͜ʖ͡°)=ε/̵͇̿̿/’̿’̿ ̿ , https://www.twitch.tv/jurgon56")
+                  print("X post sent!...")
             elif not obspython.obs_frontend_streaming_active() and sendTweet:
                   sendTweet = False
-                  print("Stream is not active")
+                  print("Stream is not active...")
 
 print(f"Start send_tweet_when_start_stream.py...")
 obspython.timer_add(timer_callback, 15 * 1000)
